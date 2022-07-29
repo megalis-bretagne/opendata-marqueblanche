f840aa3921f62b8ebdbf21a00d3f5c866cbda347
guenneuguesy <yann.guenneugues@sib.fr>
2022-07-29 09:20:48 +0000

    Problème ordre affichage des actes / annexes #20

--- resources/javascript/research.js
+++ resources/javascript/research.js
@@ -267,6 +267,9 @@ function feedResultLine(doc, collapse) {
     if (doc.siren) {
         line = line.concat('<li>SIREN : <span class="font-italic">').concat(decodeUtf8(doc.siren)).concat('</span></li>');
     }
+    if (doc.blockchain_enable && doc.blockchain_enable[0]) {
+        line = line.concat('<li><a target="_blank" href="').concat(decodeUtf8(doc.blockchain_url[0])).concat('"><i class="fa fa-cubes"></i> Lien vers la transaction</a></li>');
+    }
     line = line.concat('</ul>');
 
 
